9342987236551ab2f8a991768650aaa408b9a945
dahead <dahead@mailbox.org>
2021-04-23 10:13:36 +0200

    Update Skype_Setup_Voicemail_AllUsers.ps1

--- Skype_Setup_Voicemail_AllUsers.ps1
+++ Skype_Setup_Voicemail_AllUsers.ps1
@@ -9,13 +9,13 @@ ForEach ($SolUser in Get-CsOnlineUser -Filter {ProvisionedPlan -ne $null} -Resul
 {
 	$textWelcome = "$companyname, " + $SolUser.DisplayName
 	$textOOF = "$companyname, " + $SolUser.DisplayName + $nobodyreachable
-	
+
 	# write-output "Setting up VoiceMail OOF text for user" $SolUser.DisplayName
 	# Write-Host -NoNewLine 'Press any key to continue...';
 	# $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
-    Grant-CsConferencingPolicy -Identity $SolUser.UserPrincipalName -Verbose -PolicyName BposSDataProtection
-    Set-CsOnlineVoicemailUserSettings -Identity $SolUser.UserPrincipalName -PromptLanguage $language -VoicemailEnabled $true -OofGreetingEnabled $false -OofGreetingFollowCalendarEnabled $true -OofGreetingFollowAutomaticRepliesEnabled $true
+    	Grant-CsConferencingPolicy -Identity $SolUser.UserPrincipalName -Verbose -PolicyName BposSDataProtection
+   	Set-CsOnlineVoicemailUserSettings -Identity $SolUser.UserPrincipalName -PromptLanguage $language -VoicemailEnabled $true -OofGreetingEnabled $false -OofGreetingFollowCalendarEnabled $true -OofGreetingFollowAutomaticRepliesEnabled $true
 	
 	# Set text
 	Set-CsOnlineVoicemailUserSettings -Identity $SolUser.UserPrincipalName -DefaultGreetingPromptOverwrite $textWelcome
